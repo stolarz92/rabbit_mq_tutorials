ced3c85c0750b2a8ad85b839af7bc661e68b89a1
stolarz <stolarz92@gmail.com>
2022-07-31 18:06:16 +0200

    Add message ack (acknowledgement)

--- 2 Work queues/worker.rb
+++ 2 Work queues/worker.rb
@@ -9,11 +9,18 @@ queue = channel.queue("hello")
 puts ' [*] Waiting for messages. To exit press CTRL+C'
 
 begin
-  queue.subscribe(block: true) do |delivery_info, _properties, body|
+  # using the :manual_ack option and send a proper acknowledgment from the worker, 
+  # once we're done with a task.
+  queue.subscribe(manual_ack: true, block: true) do |delivery_info, _properties, body|
     puts " [x] Received #{body}"
 
     sleep body.count('.').to_i
     puts " [x] Done"
+
+    # An ack(nowledgement) is sent back by the consumer to tell RabbitMQ 
+    # that a particular message has been received, processed 
+    # and that RabbitMQ is free to delete it.
+    channel.ack(delivery_info.delivery_tag)
   end
 rescue Interrupt => _
   connection.close
